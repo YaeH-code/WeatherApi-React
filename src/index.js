67b5932be84896ca6695c58164a6fff3c01504f0
umhra <umehara_cyrille@outlook.com>
2025-08-12 22:11:28 +0200

    Fix method declaration

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { store } from './redux/store';
-import { Provider } from 'react-redux'
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  AppWrapper()
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 const AppWrapper = () => {
   return (
     <Provider store={store}>
-    <App />
-  </Provider>
-  )
-}
+      <App />
+    </Provider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(AppWrapper());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
